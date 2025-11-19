4ceb8d9da3e28c0c0a807a3a0b805e9d8068170e
PabloCatalan <pablocatalanfdez@gmail.com>
2025-11-19 11:19:56 +0000

    Deploying to gh-pages from @ PabloCatalan/pablocatalan.github.io@be0a4aff9e051bb5d7129750df0c67ba5c044490 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses I have taught at UC3M (under construction)",
+          description: "Materials for courses I have taught at UC3M",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
